78c726723930c2b00df33b592d6e266a6bf9f236
D3V1L#9045 <jass150505@gmail.com>
2023-07-12 22:24:48 +0100

    Integrated Loading bar, added Hover animation to links, improved user experience

--- js/js.js
+++ js/js.js
@@ -10,3 +10,64 @@
 document.addEventListener('contextmenu', 
      event => event.preventDefault()
 );*/
+
+
+/*
+
+/*Test console log when button pressed*/
+/*
+function test(){
+      console.log("test");
+}
+
+/*call test() when space bar is pressed*/
+/*
+document.addEventListener('keydown', function(event) {
+      if(event.keyCode == 32) {
+            test();
+      }
+});
+*/
+
+/*JS for progress bar that loads when page is loaded*/
+window.onload = function() {
+      var elem = document.getElementById("myBar");
+      var width = 1;
+      var id = setInterval(frame, 10);
+      function frame() {
+            if (width >= 100) {
+                  clearInterval(id);
+                  /*make bar disappear when page is loaded*/
+                  elem.style.display = "none";
+                  
+            } else {
+                  width++;
+                  elem.style.width = width + "%";
+            }
+      }
+      
+};
+
+
+/*Loading bar*/
+/*
+var i = 0;
+function move() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("myBar");
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+      }
+    }
+  }
+}
+
+*/
