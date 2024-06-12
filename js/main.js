4e5fe3e2c4ddeab097733ef1bdb496f4db257dc9
J.S.T <jass150505@gmail.com>
2024-06-12 23:13:30 +0100

    New Project Page, updated text content with fixed spelling and grammer, addded 3 old projects, changed project card padding layout

--- js/main.js
+++ js/main.js
@@ -2,3 +2,14 @@ function scrollToAbout() {
     const aboutSection = document.getElementById("about");
     aboutSection.scrollIntoView({ behavior: "smooth" });
 }
+
+function MoreProjects(){
+    //function than sends user to a new page with more projects
+    window.location.href = "more_projects.html";
+
+}
+
+function Back_Home(){
+    //function than sends user back to the home page
+    window.location.href = "index.html";
+}
